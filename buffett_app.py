3397b64bd9c4182de84221cd349f45ff33c15c87
pbes2812 <peterbenjaminsvensson@gmail.com>
2025-06-21 21:43:34 +0200

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -77,7 +77,7 @@ def ask_buffett_like_ai(data):
     """
 
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo"
         messages=[{"role": "user", "content": prompt}]
     )
 
